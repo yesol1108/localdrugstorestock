b0433d8748459313296099bf7342b3493d15d78b
Yesol Lee <lotrob88@gmail.com>
2020-03-11 04:59:22 +0900

    Updates

---
+++ precache-manifest.3ac59b0bf99a47d1116e3da4d6dabca8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37541b6b070520de506f1bbff4afab3",
+    "url": "/localdrugstorestock/index.html"
+  },
+  {
+    "revision": "fcb3516b3533cc030f47",
+    "url": "/localdrugstorestock/static/css/2.55ace5d9.chunk.css"
+  },
+  {
+    "revision": "c82788e86d9096ef0824",
+    "url": "/localdrugstorestock/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fcb3516b3533cc030f47",
+    "url": "/localdrugstorestock/static/js/2.c4e1a549.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/localdrugstorestock/static/js/2.c4e1a549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c82788e86d9096ef0824",
+    "url": "/localdrugstorestock/static/js/main.e2e0c8ba.chunk.js"
+  },
+  {
+    "revision": "9690a785518a0a2d6429",
+    "url": "/localdrugstorestock/static/js/runtime-main.6b21a999.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/localdrugstorestock/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/localdrugstorestock/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/localdrugstorestock/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/localdrugstorestock/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/localdrugstorestock/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/localdrugstorestock/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/localdrugstorestock/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/localdrugstorestock/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/localdrugstorestock/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/localdrugstorestock/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/localdrugstorestock/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/localdrugstorestock/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/localdrugstorestock/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/localdrugstorestock/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/localdrugstorestock/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/localdrugstorestock/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
